b488591117eadfef481848ac6d21e7f730a5b4f9
elliottdombrowski <elliottroyal@me.com>
2021-10-03 20:11:08 -0500

    Added basic constructor functions and classes

---
+++ index.js
@@ -0,0 +1,9 @@
+//Including required packages 
+const Inq = require('inquirer');
+const fs = require('fs');
+
+//Importing classes
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
